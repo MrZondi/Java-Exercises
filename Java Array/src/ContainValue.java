fec8e0e3a215c3423dcadf92ed22e2dffbb3600b
MrZondi <99998502+MrZondi@users.noreply.github.com>
2023-08-03 20:10:38 +0200

    Add files via upload

---
+++ Java Array/src/ContainValue.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * To contain value program
+ * Test if an array contains a specific value
+ * @author Sibulelo Zondi
+ */
+public class ContainValue {
+
+    /**
+     * Contains value function
+     * @param arr array parameter
+     * @param value value to look for parameter
+     * @return returns a String message
+     */
+    public static String containsValue(int[] arr, int value){
+
+        if (value < 0){
+            System.out.println("Value: " + value + " can not be a negative number");
+        }
+        for (int i : arr){
+            if (i == value){
+                return value +" is found in the array.";
+            }
+        }
+        return value +" is not found in the array.";
+
+    }
+    public static void main(String[] args){
+
+        int arr[] = {1,2,3,4,5,6,7,8,9,0};
+
+        System.out.print("Enter the value to look for: ");
+
+        Scanner inputValue = new Scanner(System.in);
+        int value = inputValue.nextInt();
+
+        System.out.println(containsValue(arr,value));
+
+    }
+}
